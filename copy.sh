4657769aa3e9d6f46d5d962a8174294c3919e5df
EqVert <EqVert@outlook.com>
2024-04-17 19:09:39 +0300

    1r

--- copy.sh
+++ copy.sh
@@ -1,15 +1,20 @@
-#!Копіювання файлів користувача
+#!Копіювання файлів користувача в архів
 
 # Змінні шляхів
-SOURCE_FILE="$HOME/example.txt"
+# SOURCE_FILE="$HOME/example.txt"
 SOURCE_DIR="$HOME/docs"
 BACKUP_DIR="/backup"
 
 # Створення папки для резервних копій, якщо вона не існує
 mkdir -p "$BACKUP_DIR"
 
-# Копіювання файлів і папок у папку для резервних копій
-cp "$SOURCE_FILE" "$BACKUP_DIR"
-cp -r "$SOURCE_DIR" "$BACKUP_DIR"
+# Отримання поточної дати та часу для імені файлу
+CURRENT_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
-echo "Резервне копіювання завершено!"
+# Ім'я файлу архіву
+ARCHIVE_NAME="backup-$CURRENT_DATE.tar.gz"
+
+# Створення архіву з файлів, виключаючи .dat файли
+tar -czvf "$BACKUP_DIR/$ARCHIVE_NAME" --exclude='*.dat' -C "$SOURCE_DIR" .
+
+echo "Резервне копіювання у файл $ARCHIVE_NAME завершено!"
